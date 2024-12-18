e4dbb65f3ffb23e20f28770fda770f6acc82e9a6
MSzaniecki <257482@edu.p.lodz.pl>
2024-12-18 14:44:46 +0100

    01 Zmiana metody Policz

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+int policz(int a, int b) {
+    return a+b;
+}
 int main(void)
 {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu 2+3=",policz(2,3));
     return 0;
 }
